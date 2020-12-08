1f3552e45a0a452ffbaa083a7009f45d7235f730
thethatneverwas <75694309+thethatneverwas@users.noreply.github.com>
2020-12-08 11:32:56 -0800

    Update testenv.sh

--- testenv.sh
+++ testenv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export AWS_ACCESS_KEY_ID=AKIARWLGKZUTSBP66FE7
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=[access key]
+export AWS_SECRET_ACCESS_KEY=[secret access key]
 export HACO_AWS_REGION=us-west-2
 export HACO_AWS_BUCKET=testbucket
